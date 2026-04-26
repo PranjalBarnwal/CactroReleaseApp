cd8de357da783cbc6bf8a249ef3d04c6c65a4036
pranjalbarnwal <pranjalbarnwaldev@gmail.com>
2026-04-26 13:08:02 +0530

    Allow all origins for CORS

--- backend/server.js
+++ backend/server.js
@@ -9,23 +9,7 @@ dotenv.config({ path: '../.env' });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:3000',
-  process.env.FRONTEND_URL
-].filter(Boolean);
-
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
